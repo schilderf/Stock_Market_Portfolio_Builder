e19899c7773ae921e5d625eefcbe43d1d390a27a
schilderf <72463819+schilderf@users.noreply.github.com>
2022-01-04 22:09:09 +0100

    Implement base structure for application

--- Portfolio_Builder/MainWindow.xaml.cs
+++ Portfolio_Builder/MainWindow.xaml.cs
@@ -23,6 +23,35 @@ namespace Portfolio_Builder
         public MainWindow()
         {
             InitializeComponent();
+
+            List<Market> items = new List<Market>();
+            items.Add(new Market() { Title = "Template 1", SubSectors = { "Energie", "Landwirtschaft", "Metalle" }, TimeFrames = {"1M","2M","3M","6M","1Y","3Y","5Y" }});
+            items.Add(new Market() { Title = "Template 2", SubSectors = { "Energie", "Landwirtschaft", "Metalle" }, TimeFrames = {"1M","2M","3M","6M","1Y","3Y","5Y" }});
+            items.Add(new Market() { Title = "Template 3", SubSectors = { "Energie", "Landwirtschaft", "Metalle" }, TimeFrames = {"1M","2M","3M","6M","1Y","3Y","5Y" }});
+
+            Indices.ItemsSource = items;
+            Stocks.ItemsSource = items;
+        }
+    }
+
+    public class Market
+    {
+        public string Title { get; set; }
+        public List<string> SubSectors { get; set; }
+        public string Chart { get; set; }
+        public List<string> TimeFrames { get; set; }
+        public string Symbols { get; set; }
+        public string OpenAll { get; set; }
+
+        public Market()
+        {
+            Title = "";
+            SubSectors = new List<string>();
+            Chart = "CHART";
+            TimeFrames = new List<string>();
+            Symbols = "Ticker List";
+            OpenAll = "Alle Symbole öffnen";
+
         }
     }
 }
